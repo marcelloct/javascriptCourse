533205757da001b71d393fe73e69874a40480cbf
Marcello C <marcello_ct@hotmail.com>
2022-07-11 22:22:20 -0300

    Initial Commit

---
+++ 01-fundamentals/assignments.js
@@ -0,0 +1,5 @@
+let country = "Brazil";
+let continent = "South America";
+let population = 6250347
+
+console.log(typeof country)
---
+++ 01-fundamentals/script.js
@@ -0,0 +1,2 @@
+let js = 'amazing';
+if (js === 'amazing') console.log('Javascript is FUN!');
