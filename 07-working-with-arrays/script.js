b7e7f4ad0be999b69bfbcd60fd364b40176a3bea
Marcello C <marcello_ct@hotmail.com>
2022-09-10 02:12:29 -0300

    files updated: script, style

--- 07-working-with-arrays/script.js
+++ 07-working-with-arrays/script.js
@@ -61,6 +61,52 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+// Creating DOM Elements
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = "";
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? "deposit" : "withdrawal";
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov}</div>
+      </div>`;
+
+    containerMovements.insertAdjacentHTML("afterbegin", html);
+  });
+};
+displayMovements(account1.movements);
+
+// console.log(containerMovements.innerHTML);
+
+const calcPrintBalance = function (movements) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+calcPrintBalance(account1.movements);
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+// Computing Usernames
+
+const createUserNames = function (acc) {
+  acc.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map(name => name[0])
+      .join("");
+  });
+};
+createUserNames(accounts);
+// console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -171,3 +217,106 @@ console.log(cursUnique);
 cursUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+// Challenge #1
+console.log("\n");
+console.log("---- Challenge #1 ----");
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrect = dogsJulia.slice();
+  dogsJuliaCorrect.splice(0, 1); // element start, number of elements to remove
+  dogsJuliaCorrect.splice(-2); // remove the last two
+  console.log(dogsJuliaCorrect);
+
+  const allDogs = dogsJuliaCorrect.concat(dogsKate);
+  console.log(allDogs);
+
+  allDogs.forEach(function (v, i) {
+    v >= 3
+      ? console.log(`Dog number ${i + 1} is an adult, and is ${v} years old`)
+      : console.log(`Dog number ${i + 1} is still a puppy`);
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+// Data Transformations - map, filter, reduce
+console.log("\n");
+console.log("---- Data Transformations - map, filter, reduce ----");
+
+// map looks like forearch loop, but with the difference that creates a new array
+// map returns a new array containing the results of applying an operation on all original array elements. e.g: current * 2
+
+// filter returns a new array containing the array elements that passed a specified test condition. e.g: current > 2
+
+// reduyce boils ('reduces') all array elements down to one single value. e.g: adding all elements together
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+// The map Method
+console.log("\n");
+console.log("---- The map Method ----");
+
+const eurToUSD = 1.1;
+
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUSD;
+// });
+
+const movementsUSD = movements.map(mov => mov * eurToUSD);
+console.log(movements);
+console.log(movementsUSD);
+
+// with for of
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUSD);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map((mov, i, arr) => {
+  return `Movement ${i + 1}: You ${
+    mov > 0 ? "deposited" : "withdrew"
+  } ${Math.abs(mov)}`;
+});
+
+console.log(movementsDescriptions);
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+// The filter Method
+console.log("\n");
+console.log("---- The filter Method ----");
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+
+///////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////
+// The reduce Method
+console.log("\n");
+console.log("---- The reduce Method ----");
+
+// different then others callback functions prviously learned
+// in reduce method the parameters order are: accumulator, current element, index, array
+// accumulator -> snowball
+const balance = movements.reduce(function (acc, cur, i, arr) {
+  console.log(`Iteration ${i}: ${acc}`);
+  return acc + cur;
+}, 0); // initial value for the accumulator
+console.log(balance);
+
+// maximum value
+const max = movements.reduce((acc, mov) => {
+  if (acc > mov) return acc;
+  else return mov;
+}, movements[0]);
+console.log(max);
