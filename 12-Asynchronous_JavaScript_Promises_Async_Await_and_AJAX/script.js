3419386e2009bc60556526fbfcc61810a53e15eb
Marcello C <marcello_ct@hotmail.com>
2022-12-26 23:24:56 -0300

    Chaining Promisses

--- 12-Asynchronous_JavaScript_Promises_Async_Await_and_AJAX/script.js
+++ 12-Asynchronous_JavaScript_Promises_Async_Await_and_AJAX/script.js
@@ -4,7 +4,27 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const renderCountry = function (data, className = '') {
+  const html = `
+     <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+    </article>
+  `;
 
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 ///////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////
 // Our First AJAX Call_ XMLHttpRequest
@@ -26,29 +46,124 @@ const getCountryData = function (country) {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
 
-    const html = `
-     <article class="country">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)}</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies[0].name
-            }</p>
-          </div>
-    </article>
-  `;
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    renderCountry(data);
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('germany');
-// Modern way
+// getCountryData('portugal');
+// getCountryData('usa');
+// getCountryData('germany');
+
+///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+// Callback Hell
+
+// Nested Callbacks, callback inside callback
+// Not a good practice, prefire use 'promises'
+
+console.log('\n');
+console.log('---- Callback Hell ----');
+
+const getCountryandNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    // console.log(this.responseText);
+
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // render country
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      // console.log(this.responseText);
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryandNeighbour('usa');
+
+///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+// Promises and FETCH API
+
+// Promise : An object that is used as a placeholder for the future result of an asynchronous operation.
+// A container for a future value. e.g: Response from AJAX call
+
+// We no longer need to rely on events and callbacks passed into asynchronous functions to handle asynchronous results
+// Insted of nesting callbacks, we can chain promises for a sequence of asynchronous operations: escaping callback hell
+
+console.log('\n');
+console.log('---- Promises and FETCH API ----');
+
+// Modern way - FETCH API
+const requestFetch = fetch(`https://restcountries.com/v2/name/portugal`);
+
+console.log(requestFetch);
+
+///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+// Consume Promises
+
+console.log('\n');
+console.log('---- Consume Promises ----');
+
+const getCountryData2 = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`) // returning a promise
+    .then(function (response) {
+      // .then handle the promise
+      console.log(response);
+      return response.json(); // call the .json method to read the data on that response object, also return a promise
+    })
+    .then(function (data) {
+      // handle the promise resulted from json method
+      console.log(data); // now we can access the data
+      renderCountry(data[0]);
+    });
+};
+
+// getCountryData2('portugal');
+
+///////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+// Chaining Promises
+
+console.log('\n');
+console.log('---- Chaining Promises ----');
+
+const getCountryDataChaining = function (country) {
+  // country 1
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+
+getCountryDataChaining('portugal');
